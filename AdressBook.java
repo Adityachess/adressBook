924e219512b74cedc3e1d963817b2477aa4aa284
Adityachess <adityadone9398@gmail.com>
2021-10-16 19:00:55 +0530

    Added:Ability to delete the contact details

--- AdressBook.java
+++ AdressBook.java
@@ -46,6 +46,27 @@ public class AdressBook {
 		LOG.info("Firstname" + Arrays.toString(Firstname));
 	}
 
+	public void delete(String name) {
+		for (int i = 0; i < 10; i++) {
+			boolean result = (Arrays.asList(Firstname).contains(name));
+			if (result == true) {
+				Firstname[i] = null;
+				Lastname[i] = null;
+				Address[i] = null;
+				phoneNumber[i] = null;
+
+				LOG.info("" + i);
+			}
+		} // boolean contains = Arrays.stream(Firstname).anyMatch("name"::equals);
+			// for(int i=0;i<10;i++)
+			// {
+			// }
+		LOG.info("Firstname" + Arrays.toString(Firstname));
+		LOG.info("Firstname" + Arrays.toString(Lastname));
+		LOG.info("Firstname" + Arrays.toString(Address));
+		LOG.info("Firstname" + Arrays.toString(phoneNumber));
+
+	}
 }
 
 class NewContact extends AdressBook {
@@ -105,6 +126,14 @@ class NewContact extends AdressBook {
 				String newname = next.nextLine();
 
 				contact.replace(name, newname);
+				break;
+
+			case 3:
+				Scanner cvb = new Scanner(System.in);
+				LOG.info("enter the name you want to rename");
+				name = cvb.nextLine();
+
+				contact.delete(name);
 				break;
 			}
 
