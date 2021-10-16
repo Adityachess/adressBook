e41cd980e20c8e7bf24148ad248a1755f7db99cf
Adityachess <adityadone9398@gmail.com>
2021-10-16 18:22:54 +0530

    Added:Checking the ability to create contacts in adress book

--- AdressBook.java
+++ AdressBook.java
@@ -2,6 +2,8 @@ package com.bridgelabz.adressbook;
 
 
 
+import java.util.Scanner;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,7 +13,28 @@ public class AdressBook {
 
 	public static void main(String[] args) {
 
-		LOG.info("Wecome to Address Book Program");
+
+			LOG.info("Wecome to address Book Program");
+			int PhoneNumber;
+			String Address;
+			String FirstName;
+			String lastName;
+
+			Scanner value = new Scanner(System.in);
+			LOG.info("Enter the first name");
+			FirstName = value.nextLine();
+			LOG.info("Enter the last name");
+			lastName = value.nextLine();
+			LOG.info("Enter the Address name");
+			Address = value.nextLine();
+			LOG.info("Enter the phone number");
+			PhoneNumber = value.nextInt();
+
+			LOG.info("fistName" + FirstName);
+			LOG.info("LastName" + lastName);
+			LOG.info("address" + Address);
+			LOG.info("PhoneNumber" + PhoneNumber);
+		}
 		
 	}
-}
+
